577fb0abce25ba4a85462558b7f38cf5f1bd7043
ethanansorge <ethanansorge@gmail.com>
2015-02-07 15:46:15 -0500

    Update Recursion.java

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -4,28 +4,39 @@ public class Recursion implements hw1 {
 	String s = "Ansorge,Ethan";
 	return s;
     }
-
+    public boolean checkIfValid(int n){
+    	if (n < 0){
+	    throw new IllegalArgumentException();
+	} 
+    }
     public int fact(int n){
+    	checkIfValid(n);
 	int x = 1;
-	if (n > 1){
+	if (n >= 2){
 	    x = n * fact(n - 1);
 	}
 	return x;
     }
     public int fib(int n){
-	if (n > 1){
+    	checkIfValid(n);
+    	if (n < 1){
+    		return 0;
+    	}else{
+	if (n >= 3){
 	    return fib(n-2) + fib(n-1);
 	}
-	return n;
+	return 1;
+    	}
     }
 
     public double sqrt(double n){
+    	checkIfValid(n);
 	double guess = n / 2;
 	return makeGuess(n, guess);
     }
 
     public double makeGuess(double n, double guess){
-	if( Math.abs(n - guess * guess) < .001){
+	if( Math.abs(n - guess * guess) < .0001){
 	    return guess;
 	}
 	return makeGuess(n,((n / guess + guess) / 2));
