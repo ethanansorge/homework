5ff09e9d044d33b8b93e27573f84cb189a963a64
ethanansorge <ethanansorge@gmail.com>
2015-03-05 22:25:09 -0500

    worked on hw

--- 06QuickSort/quicksort.java
+++ 06QuickSort/quicksort.java
@@ -1,13 +1,36 @@
+import java.util.Arrays;
 public class quicksort{
-    int partition (int[] ary, int start, int end){
-	int pivot = ary[ary.length -1];
-	int x = 0;
-	while (x < ary.length -1){
-	    if (ary[x] >= pivot){
-	    }else{
-	    }
-	    x = x + 1;
+    public static int partition (int[] ary, int start, int end){
+	int pivot = ary[end];
+	start = start + 1;
+	int [] d = new int[ary.length];
+	int z = d.length -1;
+	while (z > end){
+	    d[z] = ary[z];
+	    z = z -1;
 	}
-	return 2;
+	while (start < ary.length -1){
+	    if (ary[start] < pivot){
+		d[start] = ary[start];
+		start = start + 1;
+	    }else{
+		d[end] = ary[end];
+		end = end - 1;
+	    }
+	    System.out.println(Arrays.toString(d));
+	}
+	return d[pivot];
+    }
+    public static void main (String [] args){
+	int[] a = new int [7];
+	a[0] = 5;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 8;
+	a[4] = 9;
+	a[5] = 1;
+	a[6] = 6;
+	partition(a, 0, 6);
+	System.out.println(a);
     }
 }
