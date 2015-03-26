e56b60ba563f3ce248826dfc4d66ce0d1fb6a7ea
ethanansorge <ethanansorge@gmail.com>
2015-03-25 22:42:23 -0400

    added the skeleton

---
+++ 10MyDeque/MyDeque.java
@@ -0,0 +1,23 @@
+public class MyDeque{
+
+    public void addLast(T value){
+    }
+
+    public void addFirst(T value){
+    }
+
+    public T removeFirst(){
+    }
+
+    public T removeLast(){
+    }
+
+    public T getFirst(){
+    }
+
+    public T getLast(){
+    }
+
+    public void reSize(){
+    }
+}
