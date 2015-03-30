afaea44019aee5373c4a57fd8fa345fae6fe50b0
ethanansorge <ethanansorge@gmail.com>
2015-03-29 22:42:06 -0400

    working on deque, added resize, get first, get last, and remove last

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,4 +1,8 @@
 public class MyDeque{
+    Object[] queue;
+    int head;
+    int tail;
+    int size;
 
     public void addLast(T value){
     }
@@ -10,14 +14,34 @@ public class MyDeque{
     }
 
     public T removeLast(){
+	T temp = queue[tail];
+	queue[tail] = null;
+	size = size - 1;
+	tail = tail - 1;
     }
 
     public T getFirst(){
+	return (T) queue[head];
     }
 
     public T getLast(){
+	return (T) queue[tail];
     }
 
-    public void reSize(){
+    public void resize(){
+	Object[] newQueue = new Object[size * 2];
+	int i = 0;
+	int x = 0;
+	while (i < size - 1){
+	    if (x > size){
+		x = 0;
+	    }
+	    newQueue[i] = queue[x];
+	    i = i + 1;
+	    x = x + 1;
+	}
+	queue = newQueue;
+	head = 0;
+	tail = i - 1;
     }
 }
