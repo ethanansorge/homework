eaecbe65a23a95f6aa0a67ad9b893ec7b0fe4399
ethanansorge <ethanansorge@gmail.com>
2015-05-13 21:09:58 -0400

    added add

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,36 +1,44 @@
-Public class MyHeap{
-    public ArrayList values;
+import java.util.*;
+import java.io.*;
+public class MyHeap{
+    public ArrayList<Integer> values;
     public boolean max;
 
     public MyHeap(){
-	this.myHeap(true);
+	values = new ArrayList<Integer>();
+	max = true;
     }
     public MyHeap(boolean isMax){
-	values = new ArrayList();
+	values = new ArrayList<Integer>();
 	max = isMax;
     }
     public void add(int value){
-	if values.size
-	int i = 0;
-	while (i < values.size()){
+	values.add(value);
+	int i = values.size();
+	int temp;
+	while (i > 1){
+	    temp = values.get(i);
 	    if (max){
-		if (value > values[i]){
-
-		    return;
+		if (value < temp){		    
+		    break;
 		}
 	    }else{
-		if (value < values[i]){
-		    
-		    return;
+		if (value > temp){		    
+		    break;
 		}
 	    }
+	    i = i / 2;
 	}
+	values.add(i, value);
     }
     public int remove(){
+	return 0;
     }
     public int peek(){
+	return 0;
     }
     public String toString(){
+	return "";
     }
     
 }
