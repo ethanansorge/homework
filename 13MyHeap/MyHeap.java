04330d5d0f89722460a262c724ca278c13a4b277
ethanansorge <ethanansorge@gmail.com>
2015-05-13 21:35:08 -0400

    hey I added a better toString and added a throw error thing to remove

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -5,9 +5,7 @@ public class MyHeap{
     public boolean max;
 
     public MyHeap(){
-	values = new ArrayList<Integer>();
-	values.add(0);
-	max = true;
+	this(true);
     }
     public MyHeap(boolean isMax){
 	values = new ArrayList<Integer>();
@@ -34,6 +32,9 @@ public class MyHeap{
 	values.add(i, value);
     }
     public int remove(){
+	if (values.size() == 1){
+	    throw new NoSuchElementException();
+	}
 	if (max){
 	    return values.remove(1);
 	}else{
@@ -44,23 +45,29 @@ public class MyHeap{
 	return values.get(1);
     }
     public String toString(){
-        int i = 1;
+        int index = 1;
+	int newLineCounter = 1;
+	int positionOnLine = 1;
 	String s = "";
-	while (i < values.size()){
-	    s = s + values.get(i) + ", ";
-	    i = i + 1;
+	while (index < values.size()){
+	    s = s + values.get(index) + " ";
+	    if (positionOnLine == newLineCounter){
+		s = s + "\n";
+		newLineCounter = newLineCounter + 2;
+		positionOnLine = 1;
+	    }
+	    index = index + 1;
+	    positionOnLine = positionOnLine + 1;
 	}
 	return s;
 	    
     }
     public static void main(String [] args){
 	MyHeap heap = new MyHeap(true);
-	heap.add(1);
-	heap.add(7);
-	heap.add(6);
-	heap.add(9);
+	//heap.add(1);
+	System.out.println(heap.toString());
 	heap.remove();
-	System.out.println(heap.peek());
+	System.out.println(heap.toString());
     }
     
 }
